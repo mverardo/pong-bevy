146a57a6d5c9ef985a3973632126e5215b14d906
mverardo <mauricio.verardo@gmail.com>
2020-12-29 23:10:02 -0300

    Spawn a random

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use bevy::prelude::*;
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands
+        .spawn(Camera2dBundle::default())
+        .spawn(CameraUiBundle::default())
+        .spawn(SpriteBundle {
+            material: materials.add(Color::rgb(0.0, 100.0, 0.0).into()),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            sprite: Sprite::new(Vec2::new(120.0, 30.0)),
+            ..Default::default()
+        });
+}
