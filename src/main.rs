ad0cfdaea95a10e35e17624db6faa46f2724a47c
mverardo <mauricio.verardo@gmail.com>
2020-12-29 23:21:20 -0300

    Ported everything to bevy_0.4

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,48 @@ fn main() {
 }
 
 fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let wall_material = materials.add(Color::rgb(0.5, 0.5, 0.5).into());
+    let wall_thickness = 10.0;
+    let bounds = Vec2::new(900.0, 600.0);
+
+    let paddle_width = 20.0;
+    let paddle_height = 100.0;
+
     commands
         .spawn(Camera2dBundle::default())
         .spawn(CameraUiBundle::default())
+        // Walls
+        // top
         .spawn(SpriteBundle {
-            material: materials.add(Color::rgb(0.0, 100.0, 0.0).into()),
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
-            sprite: Sprite::new(Vec2::new(120.0, 30.0)),
+            material: wall_material.clone(),
+            transform: Transform::from_translation(Vec3::new(0.0, bounds.y / 2.0, 0.0)),
+            sprite: Sprite::new(Vec2::new(bounds.x + wall_thickness, wall_thickness)),
+            ..Default::default()
+        })
+        //bottom
+        .spawn(SpriteBundle {
+            material: wall_material,
+            transform: Transform::from_translation(Vec3::new(0.0, -bounds.y / 2.0, 0.0)),
+            sprite: Sprite::new(Vec2::new(bounds.x + wall_thickness, wall_thickness)),
+            ..Default::default()
+        })
+        // Paddles
+        // Player 1
+        .spawn(SpriteBundle {
+            material: materials.add(Color::rgb(0.2, 0.2, 0.8).into()),
+            transform: Transform::from_translation(Vec3::new(-bounds.x / 2.0, 0.0, 0.0)),
+            sprite: Sprite::new(Vec2::new(paddle_width, paddle_height)),
+            ..Default::default()
+        })
+        //Player 2
+        .spawn(SpriteBundle {
+            material: materials.add(Color::rgb(0.8, 0.2, 0.2).into()),
+            transform: Transform::from_translation(Vec3::new(
+                (bounds.x / 2.0) - paddle_width,
+                0.0,
+                0.0,
+            )),
+            sprite: Sprite::new(Vec2::new(paddle_width, paddle_height)),
             ..Default::default()
         });
 }
