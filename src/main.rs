2268445adcc3dc537297ad8504f90631e01b423a
mverardo <mauricio.verardo@gmail.com>
2021-01-03 22:24:39 -0300

    Ball starts walking to some direction

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,68 @@
+use std::iter::repeat_with;
+
 use bevy::prelude::*;
+use rand::{distributions::Standard, prelude::Distribution, Rng};
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    UpRight,
+    DownRight,
+    DownLeft,
+    UpLeft,
+}
+
+impl From<Direction> for Vec3 {
+    fn from(d: Direction) -> Self {
+        match d {
+            Direction::Up => Vec3::new(0.0, 1.0, 0.0),
+            Direction::Down => Vec3::new(0.0, -1.0, 0.0),
+            Direction::Left => Vec3::new(-1.0, 0.0, 0.0),
+            Direction::Right => Vec3::new(1.0, 0.0, 0.0),
+            Direction::UpRight => Vec3::new(1.0, 1.0, 0.0).normalize(),
+            Direction::DownRight => Vec3::new(1.0, -1.0, 0.0).normalize(),
+            Direction::DownLeft => Vec3::new(-1.0, -1.0, 0.0).normalize(),
+            Direction::UpLeft => Vec3::new(-1.0, 1.0, 0.0).normalize(),
+        }
+    }
+}
+
+impl Distribution<Direction> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Direction {
+        match rng.gen_range(0..8) {
+            0 => Direction::Up,
+            1 => Direction::Down,
+            2 => Direction::Left,
+            3 => Direction::Right,
+            4 => Direction::UpRight,
+            5 => Direction::DownRight,
+            6 => Direction::DownLeft,
+            7 => Direction::UpLeft,
+            _ => panic!("Random out of range 0..7"),
+        }
+    }
+}
 
 struct Ball {
-    velocity: Vec3,
+    direction: Direction,
+    speed: f32,
+}
+
+impl Ball {
+    fn new() -> Self {
+        let direction = repeat_with(rand::random::<Direction>)
+            .take_while(|d| *d != Direction::Up && *d != Direction::Down)
+            .next()
+            .expect("Somehow we generated a None starter Direction for the ball");
+
+        Ball {
+            direction,
+            speed: 300.0,
+        }
+    }
 }
 
 fn main() {
@@ -14,7 +75,9 @@ fn main() {
 
 fn ball_movement(time: Res<Time>, mut balls: Query<(&Ball, &mut Transform)>) {
     for (ball, mut transform) in balls.iter_mut() {
-        let distance = ball.velocity * time.delta_seconds();
+        let direction: Vec3 = ball.direction.into();
+        let velocity: Vec3 = ball.speed * direction;
+        let distance: Vec3 = velocity * time.delta_seconds();
 
         transform.translation += distance;
     }
@@ -75,7 +138,5 @@ fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) 
             sprite: Sprite::new(Vec2::new(ball_width, ball_height)),
             ..Default::default()
         })
-        .with(Ball {
-            velocity: Vec3::new(300.0, 0.0, 0.0),
-        });
+        .with(Ball::new());
 }
