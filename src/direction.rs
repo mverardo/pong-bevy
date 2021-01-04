f501cfb32f4a647617b778c41a6d80ab45457e27
mverardo <mauricio.verardo@gmail.com>
2021-01-03 22:35:56 -0300

    Extract direction mod and fix bud on ball startup

---
+++ src/direction.rs
@@ -0,0 +1,45 @@
+use bevy::prelude::Vec3;
+use rand::{distributions::Standard, prelude::Distribution, Rng};
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    UpRight,
+    DownRight,
+    DownLeft,
+    UpLeft,
+}
+
+impl From<Direction> for Vec3 {
+    fn from(d: Direction) -> Self {
+        match d {
+            Direction::Up => Vec3::new(0.0, 1.0, 0.0),
+            Direction::Down => Vec3::new(0.0, -1.0, 0.0),
+            Direction::Left => Vec3::new(-1.0, 0.0, 0.0),
+            Direction::Right => Vec3::new(1.0, 0.0, 0.0),
+            Direction::UpRight => Vec3::new(1.0, 1.0, 0.0).normalize(),
+            Direction::DownRight => Vec3::new(1.0, -1.0, 0.0).normalize(),
+            Direction::DownLeft => Vec3::new(-1.0, -1.0, 0.0).normalize(),
+            Direction::UpLeft => Vec3::new(-1.0, 1.0, 0.0).normalize(),
+        }
+    }
+}
+
+impl Distribution<Direction> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Direction {
+        match rng.gen_range(0..8) {
+            0 => Direction::Up,
+            1 => Direction::Down,
+            2 => Direction::Left,
+            3 => Direction::Right,
+            4 => Direction::UpRight,
+            5 => Direction::DownRight,
+            6 => Direction::DownLeft,
+            7 => Direction::UpLeft,
+            _ => panic!("Random out of range 0..7"),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,18 @@
+use bevy::{
+    core::Time,
+    ecs::{IntoSystem, Query, Res},
+    prelude::Transform,
+    prelude::{
+        App, Assets, Camera2dBundle, CameraUiBundle, Color, ColorMaterial, Commands,
+        DefaultPlugins, ResMut, SpriteBundle, Vec2, Vec3,
+    },
+    sprite::Sprite,
+};
+
+mod direction;
+use crate::direction::*;
+
 use std::iter::repeat_with;
-
-use bevy::prelude::*;
-use rand::{distributions::Standard, prelude::Distribution, Rng};
-
-#[derive(Copy, Clone, Debug, PartialEq)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-    UpRight,
-    DownRight,
-    DownLeft,
-    UpLeft,
-}
-
-impl From<Direction> for Vec3 {
-    fn from(d: Direction) -> Self {
-        match d {
-            Direction::Up => Vec3::new(0.0, 1.0, 0.0),
-            Direction::Down => Vec3::new(0.0, -1.0, 0.0),
-            Direction::Left => Vec3::new(-1.0, 0.0, 0.0),
-            Direction::Right => Vec3::new(1.0, 0.0, 0.0),
-            Direction::UpRight => Vec3::new(1.0, 1.0, 0.0).normalize(),
-            Direction::DownRight => Vec3::new(1.0, -1.0, 0.0).normalize(),
-            Direction::DownLeft => Vec3::new(-1.0, -1.0, 0.0).normalize(),
-            Direction::UpLeft => Vec3::new(-1.0, 1.0, 0.0).normalize(),
-        }
-    }
-}
-
-impl Distribution<Direction> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Direction {
-        match rng.gen_range(0..8) {
-            0 => Direction::Up,
-            1 => Direction::Down,
-            2 => Direction::Left,
-            3 => Direction::Right,
-            4 => Direction::UpRight,
-            5 => Direction::DownRight,
-            6 => Direction::DownLeft,
-            7 => Direction::UpLeft,
-            _ => panic!("Random out of range 0..7"),
-        }
-    }
-}
-
 struct Ball {
     direction: Direction,
     speed: f32,
@@ -54,8 +21,7 @@ struct Ball {
 impl Ball {
     fn new() -> Self {
         let direction = repeat_with(rand::random::<Direction>)
-            .take_while(|d| *d != Direction::Up && *d != Direction::Down)
-            .next()
+            .find(|d| *d != Direction::Up && *d != Direction::Down)
             .expect("Somehow we generated a None starter Direction for the ball");
 
         Ball {
