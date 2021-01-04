db0f2951eaf51329d2651d20f041bed9be4b3702
mverardo <mauricio.verardo@gmail.com>
2021-01-03 21:44:18 -0300

    Ball walks right

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use bevy::prelude::*;
 
+struct Ball {
+    velocity: Vec3,
+}
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_system(ball_movement.system())
         .run();
+}
+
+fn ball_movement(time: Res<Time>, mut balls: Query<(&Ball, &mut Transform)>) {
+    for (ball, mut transform) in balls.iter_mut() {
+        let distance = ball.velocity * time.delta_seconds();
+
+        transform.translation += distance;
+    }
 }
 
 fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
@@ -17,7 +30,6 @@ fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) 
 
     let ball_width = 20.0;
     let ball_height = 20.0;
-
 
     commands
         .spawn(Camera2dBundle::default())
@@ -59,12 +71,11 @@ fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) 
         //Ball
         .spawn(SpriteBundle {
             material: materials.add(Color::rgb(0.2, 0.2, 0.2).into()),
-            transform: Transform::from_translation(Vec3::new(
-                0.0,
-                0.0,
-                0.0,
-            )),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
             sprite: Sprite::new(Vec2::new(ball_width, ball_height)),
             ..Default::default()
+        })
+        .with(Ball {
+            velocity: Vec3::new(300.0, 0.0, 0.0),
         });
 }
